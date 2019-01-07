612801cbb9b350282f44af6dab15942ce01c215c
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2019-01-07 21:40:22 +0800

    Add handler to update session/break length

--- src/App.js
+++ src/App.js
@@ -5,12 +5,41 @@ import Timer from './components/Timer';
 import TimerController from './components/TimerController';
 
 class App extends Component {
+  state = {
+    sessionTime: 25,
+    breakTime: 5
+  };
+
+  handleUpdateLength = (type, action) => {
+    const timeType = type === 'session' ? 'sessionTime' : 'breakTime';
+
+    if (action === 'inc') {
+      this.setState(prevState => ({
+        [timeType]: prevState[timeType] + 1
+      }));
+    } else if (action === 'dec') {
+      this.setState(prevState => ({
+        [timeType]: prevState[timeType] - 1
+      }));
+    }
+  };
+
   render() {
+    const { sessionTime, breakTime } = this.state;
+
     return (
       <div className="App">
         <div>Pomodoro Clock</div>
-        <LengthController type="session" length={25} />
-        <LengthController type="break" length={5} />
+        <LengthController
+          type="session"
+          length={sessionTime}
+          onUpdateLength={this.handleUpdateLength}
+        />
+        <LengthController
+          type="break"
+          length={breakTime}
+          onUpdateLength={this.handleUpdateLength}
+        />
         <Timer timerType="Session" />
         <TimerController />
       </div>
--- src/components/LengthController.js
+++ src/components/LengthController.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const LengthController = ({ type, length }) => (
+const LengthController = ({ type, length, onUpdateLength }) => (
   <div className="length-ctrl">
     <div id={`${type}-label`}>{`${type} Length`}</div>
     <div className="length-buttons">
-      <button id={`${type}-increment`}> + </button>
+      <button
+        id={`${type}-increment`}
+        onClick={() => onUpdateLength(type, 'inc')}
+        disabled={length >= 60 ? true : false}
+      >
+        +
+      </button>
       <div id={`${type}-length`}>{length}</div>
-      <button id={`${type}-decrement`}>-</button>
+      <button
+        id={`${type}-decrement`}
+        onClick={() => onUpdateLength(type, 'dec')}
+        disabled={length <= 1 ? true : false}
+      >
+        -
+      </button>
     </div>
   </div>
 );
 
 LengthController.propTypes = {
   type: PropTypes.string.isRequired,
-  length: PropTypes.number.isRequired
+  length: PropTypes.number.isRequired,
+  onUpdateLength: PropTypes.func.isRequired
 };
 
 export default LengthController;
