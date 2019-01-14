d7eb980c323088e944423c49edaffe3ac8742f03
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2019-01-14 22:17:20 +0800

    Updates

---
+++ precache-manifest.498135deb1551b10a892924914b93eb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f41d346ce9ebc121a229",
+    "url": "/pomodoro-clock/static/js/runtime~main.f41d346c.js"
+  },
+  {
+    "revision": "0774af8dcbb4eae65ad4",
+    "url": "/pomodoro-clock/static/js/main.0774af8d.chunk.js"
+  },
+  {
+    "revision": "d439e8744bee3815eb5d",
+    "url": "/pomodoro-clock/static/js/1.d439e874.chunk.js"
+  },
+  {
+    "revision": "0774af8dcbb4eae65ad4",
+    "url": "/pomodoro-clock/static/css/main.2a873e89.chunk.css"
+  },
+  {
+    "revision": "cbe4f4cb7d660e473c8bf5a65368c401",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
